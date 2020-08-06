a21de49901f2433e0632df92faf1e4e786ab737c
islam elhady <i.elhady44@gmail.com>
2020-08-06 22:46:35 +0200

    change entity table

---
+++ app/src/main/java/com/example/elhady/notes/models/Note.java
@@ -0,0 +1,106 @@
+package com.example.elhady.notes.models;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity(tableName = "notes")
+public class Note implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id ;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "data_time")
+    private String dataTime;
+
+    @ColumnInfo(name = "subtitle")
+    private String subtitle;
+
+    @ColumnInfo(name = "note_text")
+    private String noteText;
+
+    @ColumnInfo(name = "image_path")
+    private String imagePath;
+
+    @ColumnInfo(name = "color")
+    private String color;
+
+    @ColumnInfo(name = "web_link")
+    private String webLink;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDataTime() {
+        return dataTime;
+    }
+
+    public void setDataTime(String dataTime) {
+        this.dataTime = dataTime;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getNoteText() {
+        return noteText;
+    }
+
+    public void setNoteText(String noteText) {
+        this.noteText = noteText;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getWebLink() {
+        return webLink;
+    }
+
+    public void setWebLink(String webLink) {
+        this.webLink = webLink;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return title + " : " + dataTime;
+    }
+}
