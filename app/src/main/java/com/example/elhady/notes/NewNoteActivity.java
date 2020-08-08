075044845e4fdf76c334dda4b9af86844d1b0313
islam elhady <i.elhady44@gmail.com>
2020-08-08 02:47:38 +0200

    u can save note

--- app/src/main/java/com/example/elhady/notes/NewNoteActivity.java
+++ app/src/main/java/com/example/elhady/notes/NewNoteActivity.java
@@ -1,17 +1,30 @@
 package com.example.elhady.notes;
 
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.el_hady.notes.R;
+import com.example.elhady.notes.database.NoteDatabase;
+import com.example.elhady.notes.models.Note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class NewNoteActivity extends AppCompatActivity {
 
-
+    private EditText inputNoteTitle, inputNoteSubtitle, inputNoteText;
+    private TextView textDataTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,9 +35,62 @@ public class NewNoteActivity extends AppCompatActivity {
         imageBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               onBackPressed();
+                onBackPressed();
             }
         });
 
+        inputNoteTitle = findViewById(R.id.input_notes_title);
+        inputNoteSubtitle = findViewById(R.id.input_notes_subtitle);
+        inputNoteText = findViewById(R.id.input_notes);
+        textDataTime = findViewById(R.id.text_data_time);
+
+        // ex: Sunday,14 May 1995 22:34 PM
+        textDataTime.setText(new SimpleDateFormat("EEEE, dd MMMM yyyy HH:mm a", Locale.getDefault())
+                .format(new Date()));
+
+        ImageView imageSave = findViewById(R.id.image_save);
+        imageSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNote();
+            }
+        });
+
+    }
+
+    private void saveNote() {
+        if (inputNoteTitle.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note title can't be empty", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (inputNoteSubtitle.getText().toString().trim().isEmpty()
+                && inputNoteText.getText().toString().trim().isEmpty()) {
+            Toast.makeText(this, "Note can't be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        final Note note = new Note();
+        note.setTitle(inputNoteTitle.getText().toString());
+        note.setSubtitle(inputNoteSubtitle.getText().toString());
+        note.setNoteText(inputNoteText.getText().toString());
+        note.setDataTime(textDataTime.getText().toString());
+
+        @SuppressLint("StaticFieldLeak")
+        class SaveNoteTask extends AsyncTask<Void, Void, Void> {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                NoteDatabase.getDatabase(getApplicationContext()).noteDao().insertNote(note);
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                super.onPostExecute(aVoid);
+                Intent intent = new Intent();
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        }
+        new SaveNoteTask().execute();
     }
 }
